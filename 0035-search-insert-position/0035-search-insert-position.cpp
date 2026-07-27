12a04d5bb9bea11686f3f4f4f311c5beefcd2a6a
yashmakode-gif <yashmakode159@gmail.com>
2026-07-27 23:59:06 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-       int n=target;
        for(int i=0;i<nums.size();i++){
         if(nums[i]>=target){
             return i;
