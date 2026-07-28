412eb5dad4bfdebde310e7762b0d082b9aaec3fa
yashmakode-gif <yashmakode159@gmail.com>
2026-07-28 22:37:55 +0530

    Time: 7 ms (13.64%), Space: 8.6 MB (37.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-     
-        if (x < 0) return false;   
-        
-        long original = x;
-        long reversed = 0;
-        
-        while (x > 0) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x = x / 10;
+        if(x < 0){
+            return false;
         }
-        
-        return original == reversed;
+        long temp = x;
+        long rem = 0;
+        long xCopy = x;
+        while(xCopy > 0){
+            rem = rem * 10 + (xCopy % 10);
+            xCopy = xCopy / 10;
+        }
+        if(rem == temp)
+            return true;
+        else
+            return false;
     }
 };
-    
+       
