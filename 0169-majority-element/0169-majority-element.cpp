cc525ccfccfee8ef8b47e16a287e348eff434bb6
yashmakode-gif <yashmakode159@gmail.com>
2026-08-06 01:11:44 +0530

    Time: 4 ms (19.19%), Space: 28.3 MB (13.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,12 +3,12 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> yash;
         int k=0;
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0;i<nums.size();i++){
             yash[nums[i]]++;
         }
 
-        for(int i=0; i<nums.size(); i++){
-            if(yash[nums[i]] > (nums.size()/2)){
+        for(int i=0;i<nums.size();i++){
+            if(yash[nums[i]]>nums.size()/2){
                 k=nums[i];
             }
         }
