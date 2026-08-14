c35dc287c9928ae04a7efbc4cd75429a999c2aa1
yashmakode-gif <yashmakode159@gmail.com>
2026-08-14 23:43:20 +0530

    Time: 0 ms (100%), Space: 21.9 MB (39.23%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(nums[0]!=0){
+            return 0;
+        }
+        if(nums[n-1] != n){
+            return n;
+            }
+                for(int i=0;i<nums.size();i++){
+                if(nums[i]!=i){
+                return i;
+                }
+ 
+        }
+        return 0;
+    }
+};
