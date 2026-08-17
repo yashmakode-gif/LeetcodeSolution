78afd7e8b63eeb15431d5f9aebdcfae9bab82333
yashmakode-gif <yashmakode159@gmail.com>
2026-08-17 19:58:35 +0530

    Time: 3 ms (9.82%), Space: 10.8 MB (43.95%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
-       int freq[101]={};
-       int sum = 0;
-
-
-        for (int x : nums)
-            freq[x]++;
-
+       unordered_map<int, int> freq;
+        int sum = 0;
         for (int x : nums) {
-            if (freq[x] == 1)
-                sum += x;
+            freq[x]++;
         }
 
+        for (int x : nums) {
+            if (freq[x] == 1) {
+                sum += x;
+            }
+        }
         return sum;
     }
 };
