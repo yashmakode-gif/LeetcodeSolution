cc16b33ca92716a1e0aa54d675f31c45b87c7115
Yash Makode <yashmakode159@gmail.com>
2026-09-25 23:10:55 +0530

    Time: 6 ms (75.55%), Space: 44.8 MB (47.71%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        int child = 0;
+        int cookie = 0;
+        
+        while (child < g.size() && cookie < s.size()) {
+            if (s[cookie] >= g[child]) {
+                child++; 
+            }
+            cookie++; 
+        }
+        
+        return child;
+    }
+};
